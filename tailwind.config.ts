fd4ac66edffae2a031e1f2fc927a77c0c54d89c5
mjfeezy <56008211+mjfeezy@users.noreply.github.com>
2026-02-05 14:15:26 -0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,6 +55,7 @@ const config: Config = {
         info: {
           50: '#eff6ff',
           100: '#dbeafe',
+          200: '#bfdbfe',
           500: '#3b82f6',
           600: '#2563eb',
           800: '#1e40af',
